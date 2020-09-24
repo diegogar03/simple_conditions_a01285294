3f5cf156dd2e31a257b6700a356b332e86b62b5d
diegogar03 <70651719+diegogar03@users.noreply.github.com>
2020-09-24 10:55:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,20 +3,11 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(celsius)
     if (celsius == 10) {
         basic.showString("Ski Time!")
-    } else {
-        basic.showNumber(celsius)
-    }
-    if (celsius == 20) {
+    } else if (celsius == 20) {
         basic.showString("Picnic Time!")
-    } else {
-        basic.showNumber(celsius)
-    }
-    if (celsius == 30) {
+    } else if (celsius == 30) {
         basic.showString("Beach Time!")
-    } else {
-        basic.showNumber(celsius)
-    }
-    if (celsius == 40) {
+    } else if (celsius == 40) {
         basic.showString("It is better to stay at home!")
     } else {
         basic.showNumber(celsius)
